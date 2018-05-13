e227cc8e3983339b6b191129d310e6946b24ded7
Filip Lange <filip.lange@gmail.com>
2018-05-13 22:25:22 +0200

    Moved functionality to RailBuilder class

--- RailBuilder.js
+++ RailBuilder.js
@@ -3,7 +3,34 @@
  */
 
 class RailBuilder {
+  constructor() {
+    this.building = false;
+    this.images = [];
+    this.positions = [];
+  }
 
+  pointerDown(coordinates) {
+    this.railBuildingStartX = coordinates.x;
+    this.railBuildingStartY = coordinates.y;
+    this.building = true
+  }
+
+  pointerUp() {
+    if (this.building) {
+      this.building = false;
+    }
+  }
+
+  pointerMove(coordinates, scope) {
+    if (this.building) {
+      let gridName = "x" + coordinates.x + "y" + coordinates.y;
+      if (!this.positions.includes(gridName)) {
+        scope.add.image(coordinates.x, coordinates.y, 'halfrail');
+      }
+      //GRID[gridName] = {name: gridName};
+      //this.add.image(x, y, 'halfrail');
+    }
+  }
 }
 
 export { RailBuilder };
