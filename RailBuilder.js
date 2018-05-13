383c9876d1d16a60b4e1e2b716d59f6e61945b61
Filip Lange <filip.lange@gmail.com>
2018-05-13 23:05:01 +0200

    RailBuilder now works almost as it should, but only horizontally

--- RailBuilder.js
+++ RailBuilder.js
@@ -2,6 +2,8 @@
  * Created by Filip on 2018-05-08.
  */
 
+const listCoordinatesFromStartTo = Symbol('listCoordinatesFromStartTo');
+
 class RailBuilder {
   constructor() {
     this.building = false;
@@ -10,27 +12,50 @@ class RailBuilder {
   }
 
   pointerDown(coordinates) {
-    this.railBuildingStartX = coordinates.x;
-    this.railBuildingStartY = coordinates.y;
+    this.startX = coordinates.x;
+    this.startY = coordinates.y;
     this.building = true
   }
 
   pointerUp() {
     if (this.building) {
       this.building = false;
+      this.images = [];
     }
   }
 
-  pointerMove(coordinates, scope) {
-    if (this.building) {
-      let gridName = "x" + coordinates.x + "y" + coordinates.y;
-      if (!this.positions.includes(gridName)) {
-        scope.add.image(coordinates.x, coordinates.y, 'halfrail');
+  [listCoordinatesFromStartTo] (coordinate, tilesize) {
+    let coordinates = [];
+    if (coordinate.y === this.startY) {
+      let step = coordinate.x > this.startX ? tilesize : -tilesize;
+      for (let x = this.startX; x != coordinate.x; x += step) {
+        coordinates.push({x: x, y: this.startY})
       }
+    }
+    // The for loop stops before adding the final coordinate, so add it here
+    coordinates.push({x: coordinate.x, y: coordinate.y});
+    return coordinates;
+  }
+
+  pointerMove(coordinates, game, TILESIZE) {
+    if (this.building) {
+      for (var image of this.images) {
+        image.destroy()
+      }
+      this.images = [];
+      for (var coordinate of this[listCoordinatesFromStartTo](coordinates, TILESIZE)) {
+        let image = game.add.image(coordinate.x, coordinate.y, 'halfrail');
+        this.images.push(image)
+      }
+      // let gridName = "x" + coordinates.x + "y" + coordinates.y;
+      // if (!this.positions.includes(gridName)) {
+      //   let image = game.add.image(coordinates.x, coordinates.y, 'halfrail');
+      //   this.images.push(image)
+      // }
       //GRID[gridName] = {name: gridName};
       //this.add.image(x, y, 'halfrail');
     }
   }
 }
 
-export { RailBuilder };
+export {RailBuilder};
