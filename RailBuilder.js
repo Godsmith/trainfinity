debd8c8da9face2ca5d225d0aac765c641f05755
Filip Lange <filip.lange@gmail.com>
2018-05-15 22:45:06 +0200

    Building horizontal lines kind of work

--- RailBuilder.js
+++ RailBuilder.js
@@ -3,12 +3,13 @@
  */
 
 const listCoordinatesFromStartTo = Symbol('listCoordinatesFromStartTo');
+const downwardsOrLeftwards = Symbol('downwardsOrLeftwards');
 
 class RailBuilder {
-  constructor() {
+  constructor(grid) {
+    this.grid = grid;
     this.building = false;
     this.images = [];
-    this.positions = [];
   }
 
   pointerDown(coordinates) {
@@ -27,6 +28,9 @@ class RailBuilder {
   [listCoordinatesFromStartTo] (coordinate, tilesize) {
     let coordinates = [];
     if (coordinate.y === this.startY) {
+      if (coordinate.x === this.startX) {
+        return [coordinate];
+      }
       let step = coordinate.x > this.startX ? tilesize : -tilesize;
       for (let x = this.startX; x != coordinate.x; x += step) {
         coordinates.push({x: x, y: this.startY})
@@ -46,13 +50,31 @@ class RailBuilder {
   }
 
   pointerMove(coordinates, game, TILESIZE) {
+    let horizontal = (coordinates.y === this.startY);
     if (this.building) {
       for (var image of this.images) {
         image.destroy()
       }
       this.images = [];
-      for (var coordinate of this[listCoordinatesFromStartTo](coordinates, TILESIZE)) {
-        let image = game.add.image(coordinate.x, coordinate.y, 'halfrail');
+      this.coordinateList = this[listCoordinatesFromStartTo](coordinates, TILESIZE);
+      if (this.coordinateList.length < 2) {
+        return;
+      }
+      for (var coordinate of this.coordinateList) {
+        let imageFileName = 'halfrail';
+        let angle = horizontal ? 90 : 0;
+        if (this[downwardsOrLeftwards](this.coordinateList)) {
+          angle += 180;
+        }
+        if (coordinate === this.coordinateList[0]) {
+          // Do nothing
+        } else if (coordinate === this.coordinateList[this.coordinateList.length - 1]) {
+          angle += 180
+        } else {
+          imageFileName = 'rail';
+        }
+        let image = game.add.image(coordinate.x, coordinate.y, imageFileName);
+        image.angle = angle;
         this.images.push(image)
       }
       // let gridName = "x" + coordinates.x + "y" + coordinates.y;
@@ -63,6 +85,15 @@ class RailBuilder {
       //GRID[gridName] = {name: gridName};
       //this.add.image(x, y, 'halfrail');
     }
+  }
+
+  [downwardsOrLeftwards] (coordinateList) {
+    if (coordinateList.length < 2) {
+      return false;
+    }
+    let first = coordinateList[0];
+    let last = coordinateList[coordinateList.length - 1];
+    return (first.x > last.x) || (first.y < last.y)
   }
 }
 
