479eb2f36dca452b97f8dd29572bd2cb76922f28
Filip Lange <filip.lange@gmail.com>
2018-07-25 23:50:46 +0200

    Created first test case

---
+++ RailSegment.test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Filip on 2018-07-25.
+ */
+import {RailSegmentFactory} from "./RailSegment.js";
+
+test('Instantiate RailSegmentFactory', () => {
+  let r = new RailSegmentFactory();
+});
