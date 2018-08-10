1e5afa458d6b3916e4a74bafb7d7b89cef02320c
Filip Lange <filip.lange@gmail.com>
2018-08-10 23:27:07 +0200

    Rudimentary support for building trains

---
+++ TrainBuilder.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Filip on 2018-08-10.
+ */
+
+import {ActionController} from "./ActionController.js"
+
+class Train {
+  constructor() {
+    this.imageName = 'train';
+    // TODO: should not always be 0 here
+    this.angle = 0;
+  }
+}
+
+class TrainBuilder extends ActionController {
+  constructor(grid) {
+    super(grid);
+  }
+
+  _positionsToMarkInvalid() {
+    return this.positions.filter((x) => !this.grid.hasRail(x));
+  }
+
+  _createBuildingSegments() {
+    this.buildingSegments = this.positions.map(() => new Train());
+  }
+}
+
+export {TrainBuilder};
