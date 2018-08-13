7fd9d850523391da88a0d5631db299e49b904555
Filip Lange <filip.lange@gmail.com>
2018-08-13 19:52:26 +0200

    Removed tint

--- Image.js
+++ Image.js
@@ -2,12 +2,11 @@
  * Created by Filip on 2018-07-26.
  */
 class Image {
-  constructor(x, y, name, angle, tint) {
+  constructor(x, y, name, angle) {
     this.x = x;
     this.y = y;
     this.name = name;
     this.angle = angle;
-    this.tint = tint;
   }
 }
 
